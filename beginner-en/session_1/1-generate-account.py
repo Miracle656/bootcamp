e57f1a00151f1c3e8176bdb5d683d9644069fae6
Miracle656 <83222244+Miracle656@users.noreply.github.com>
2023-09-09 14:01:08 +0100

    Update 1-generate-account.py

--- beginner-en/session_1/1-generate-account.py
+++ beginner-en/session_1/1-generate-account.py
@@ -1,5 +1,7 @@
 # 1. open new terminal
 # 2. install algokit_utils `pip install algokit-utils`
+import algokit_utils as algokit
+
 # 3. confirm installation `pip list` (algokit-utils & py-algorand-sdk)
 # 4. change directory to session_1 `cd beginner-en/session_1`
 # 5. launch VSCode from session_1 directory `code .`
@@ -8,7 +10,9 @@
 
 def main():
     # use new_account() method to generate a new account
+    my_account = algokit.Account.new_account()
 
     # print the address of the account object
+    print(my_account.private_key)
 
 main()
