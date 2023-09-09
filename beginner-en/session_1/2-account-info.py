c20e1a3bb4b30fb4ee815fe0b1c1b4dcf6b23dad
Miracle656 <83222244+Miracle656@users.noreply.github.com>
2023-09-09 14:01:42 +0100

    Update 2-account-info.py

--- beginner-en/session_1/2-account-info.py
+++ beginner-en/session_1/2-account-info.py
@@ -1,26 +1,45 @@
 # 1. add imports
+import algokit_utils as algokit
+from pprint import pprint
+
+
 # 2. copy/paste code from previous example above the break
 # 3. add new code below the break
 
 def main():
     # use new_account() method to generate a new account
+    my_account = algokit.Account.new_account()
 
     # print the address of the account object
+    print(my_account.address)
 
     #####################################################################
     # NEW BELOW
     #####################################################################
 
     # instatiate algod client on localnet
+    algod = algokit.get_algod_client(algokit.get_default_localnet_config('algod'))
 
     # print the version for algod
+    #pprint(algod.versions())
 
     # get info about my_account from algod
+    pprint(algod.account_info(my_account.address))
 
     # instatiate kmd client on localnet
+    kmd = algokit.get_kmd_client_from_algod_client(algod)
 
     # fund my_account with ALGO from localnet dispenser
+    algokit.ensure_funded(
+        algod,
+        algokit.EnsureBalanceParameters(
+            account_to_fund=my_account,
+            min_spending_balance_micro_algos=1_000_000 #micro algos
+        )
+    )
 
     # again, get info about my_account from algod
+    pprint(algod.account_info(my_account.address))
+
 
 main()
